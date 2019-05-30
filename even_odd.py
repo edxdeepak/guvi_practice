041c65ac941d74d3c1d000f0e08a6fc514c59c33
edxdeepak <51188497+edxdeepak@users.noreply.github.com>
2019-05-30 16:07:39 +0530

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,5 +1,8 @@
-number = int(input('Enter the number'))
-if number % 2 == 0:
-  print('Even')
-else:
-  print('Odd')
+try:
+  number = int(input('Enter the number'))
+  if number % 2 == 0:
+    print('Even')
+  else:
+    print('Odd')
+except:
+  print('Invalid Input')
