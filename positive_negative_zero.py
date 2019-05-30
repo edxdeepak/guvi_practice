140739576f707e81a4fc89084da937a2d991b9f9
edxdeepak <51188497+edxdeepak@users.noreply.github.com>
2019-05-30 15:34:49 +0530

    Update positive_negative_zero.py

--- positive_negative_zero.py
+++ positive_negative_zero.py
@@ -1,10 +1,7 @@
-number = input('Enter the number')
-if number.isnumeric():
-  if int(number) == 0:
+number = int(input('Enter the number'))
+if (number) == 0:
     print('Zero')
-  elif int(number) > 0:
+elif (number) > 0:
     print('Positive')
-  else:
-    print('Negative')
 else:
-  print('Invalid Input')
+    print('Negative')
